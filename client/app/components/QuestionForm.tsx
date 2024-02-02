15c0255a6f30635bb23a5018ec52f24d13e7a299
PandaGuerrier <76222146+PandaGuerrier@users.noreply.github.com>
2024-02-02 09:29:51 +0100

    Create QuestionForm.tsx

---
+++ client/app/components/QuestionForm.tsx
@@ -0,0 +1,33 @@
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+export default function QuestionFormComponent() {
+  return (
+    <div>
+      <form>
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>This is your public display name.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+      </form>
+    </div>
+  )
+}
